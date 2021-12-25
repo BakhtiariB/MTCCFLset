31cbe1d8cab62a913a408192e09e03273a6ad371
BakhtiariB <91058490+BakhtiariB@users.noreply.github.com>
2021-12-25 11:52:35 +0330

    Update main.m

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%       ---MTCCFLset---
 clc
 clear all
-fid=fopen('F:\KABA_Feature Set\MTCCFLset Feature Set\MTCCFLset Feature Set\emob_Large.arff');
+fid=fopen('...\emob_Large.arff');
 MTCCFLset(fid);
